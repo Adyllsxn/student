3008d5143dcba61f47dd677f41ad9daa6da24498
Adyllson <domingosady11son@gmail.com>
2025-04-16 11:02:31 +0100

    adiconando os projetos de tests

---
+++ test/Student.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Student.IntegrationTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/Student.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Student.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
