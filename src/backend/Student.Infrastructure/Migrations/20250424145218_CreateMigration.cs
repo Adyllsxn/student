1382fad1a702afc8b74612e20cbfd898c3195fec
Adyllson <domingosady11son@gmail.com>
2025-04-24 15:54:14 +0100

    gerando a migration e fazendo o upadate para bd

---
+++ src/backend/Student.Infrastructure/Migrations/20250424145218_CreateMigration.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Student.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tbl_Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tbl_Categoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tbl_Postagem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "NVARCHAR(50)", maxLength: 50, nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Imagem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tbl_Postagem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CategoriaPostagem",
+                        column: x => x.CategoriaId,
+                        principalTable: "Tbl_Categoria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tbl_Categoria",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "Programação" },
+                    { 2, "Design" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tbl_Postagem_CategoriaId",
+                table: "Tbl_Postagem",
+                column: "CategoriaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tbl_Postagem");
+
+            migrationBuilder.DropTable(
+                name: "Tbl_Categoria");
+        }
+    }
+}
