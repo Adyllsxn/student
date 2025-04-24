1168074d3cb1e06b0140497537172239406cd8bc
Adyllson <domingosady11son@gmail.com>
2025-04-24 16:27:34 +0100

    finalizando a codificação do repositorio das paostagens

--- src/backend/Student.Infrastructure/Repositories/PostagemRepository.cs
+++ src/backend/Student.Infrastructure/Repositories/PostagemRepository.cs
@@ -1,38 +1,264 @@
 namespace Student.Infrastructure.Repositories;
-public class PostagemRepository : IPostagemRepository
+public class PostagemRepository(AppDbContext context) : IPostagemRepository
 {
-    public Task<Result<PostagemEntity>> CreateAsync(PostagemEntity entity, CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+    #region </Create>
+        public async Task<Result<PostagemEntity>> CreateAsync(PostagemEntity entity, CancellationToken token)
+        {
+            try
+            {
+                if(entity == null)
+                {
+                    return new Result<PostagemEntity>(
+                        null, 
+                        400, 
+                        "Parâmetros não podem estar vazio."
+                        );
+                }
+                await context.Postagens.AddAsync(entity, token);
+                return new Result<PostagemEntity>(
+                    entity, 
+                    201, 
+                    "Operação executada com sucesso."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<PostagemEntity>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (CRIAR). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
 
-    public Task<Result<bool>> DeleteAsync(int entityId, CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+    #region </Delete>
+        public async Task<Result<bool>> DeleteAsync(int entityId, CancellationToken token)
+        {
+            try
+            {
+                if (entityId <= 0)
+                {
+                    return new Result<bool>(
+                        false, 
+                        400, 
+                        "ID deve ser maior que zero."
+                        );
+                }
+                var response = await context.Postagens.FindAsync(entityId, token);
+                if (response == null)
+                {
+                    return new Result<bool>(
+                        false, 
+                        404, 
+                        "ID não encontrado."
+                        );
+                }
+                context.Postagens.Remove(response);
+                return new Result<bool>(
+                    true, 
+                    200, 
+                    "Operação executada com sucesso."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<bool>(
+                    false, 
+                    500, 
+                    $"Erro ao executar a operação (DELETAR). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
 
-    public Task<PagedList<List<PostagemEntity>?>> GetAllAsync(CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+    #region </GetAll>
+        public async Task<PagedList<List<PostagemEntity>?>> GetAllAsync(PagedRequest request, CancellationToken token)
+        {
+            try
+            {
+                var query = context.Postagens.AsNoTracking().AsQueryable();
 
-    public Task<Result<PostagemEntity?>> GetByIdAsync(int entityId, CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+                var result = await query
+                            .Skip((request.PageNumber - 1) * request.PageSize)
+                            .Take(request.PageSize)
+                            .ToListAsync();
+                
+                var count = await query.CountAsync();
 
-    public Task<Result<PostagemEntity?>> GetFileAsync(int entityId, CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+                return new PagedList<List<PostagemEntity>?>(
+                    result,
+                    count,
+                    request.PageNumber,
+                    request.PageSize
+                );
+            }
+            catch (Exception ex)
+            {
+                return new PagedList<List<PostagemEntity>?>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (CRIAR). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
 
-    public Task<PagedList<List<PostagemEntity>?>> SearchAsync(Expression<Func<PostagemEntity, bool>> expression, string entity, CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+    #region </GetById>
+        public async Task<Result<PostagemEntity?>> GetByIdAsync(int entityId, CancellationToken token)
+        {
+            try
+            {
+                if(entityId <= 0)
+                {
+                    return new Result<PostagemEntity?>(
+                        null, 
+                        400, 
+                        "ID deve ser maior que zero."
+                        );
+                }
+                var response = await context.Postagens.FindAsync(entityId, token);
+                if(response == null)
+                {
+                    return new Result<PostagemEntity?>(
+                        null, 
+                        404, 
+                        "ID não encontrado."
+                        );
+                }
+                return new Result<PostagemEntity?>(
+                    response, 
+                    200, 
+                    "Dados encontrado."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<PostagemEntity?>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (GET BY ID). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
 
-    public Task<Result<PostagemEntity>> UpdateAsync(PostagemEntity entity, CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+    #region </GetFile>
+        public async Task<Result<PostagemEntity?>> GetFileAsync(int entityId, CancellationToken token)
+        {
+            try
+            {
+                if(entityId <= 0)
+                {
+                    return new Result<PostagemEntity?>(
+                        null, 
+                        400, 
+                        "ID deve ser maior que zero."
+                        );
+                }
+                var response = await context.Postagens.FindAsync(entityId, token);
+                if(response == null)
+                {
+                    return new Result<PostagemEntity?>(
+                        null, 
+                        404, 
+                        "ID não encontrado."
+                        );
+                }
+                return new Result<PostagemEntity?>(
+                    response, 
+                    200, 
+                    "Dados encontrado."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<PostagemEntity?>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (GET BY ID). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+
+    #region </Search>
+        public async Task<Result<List<PostagemEntity>?>> SearchAsync(Expression<Func<PostagemEntity, bool>> expression, string entity, CancellationToken token)
+        {
+            try
+            {
+                if(entity == null)
+                {
+                    return new Result<List<PostagemEntity>?>(
+                        null, 
+                        400, 
+                        "Parâmetros não podem estar vazio."
+                        );
+                }
+                var response = await context.Postagens.Where(expression).ToListAsync(token);
+                if(response == null || response.Count == 0)
+                {
+                    return new Result<List<PostagemEntity>?>(
+                        null, 
+                        404, 
+                        "Nenhum dado encontrado."
+                        );
+                }
+
+                return new Result<List<PostagemEntity>?>(
+                    response, 
+                    200, 
+                    "Dados encontrado."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<List<PostagemEntity>?>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (SEARCH). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+
+    #region </Update>
+        public async Task<Result<PostagemEntity>> UpdateAsync(PostagemEntity entity, CancellationToken token)
+        {
+            try
+            {
+                if(entity == null)
+                {
+                    return new Result<PostagemEntity>(
+                        null, 
+                        400, 
+                        "Parâmetros não podem estar vazio."
+                        );
+                }
+                var response = await context.Postagens.FindAsync(entity.Id);
+                if(response == null)
+                {
+                    return new Result<PostagemEntity>(
+                        null, 
+                        404, 
+                        "ID não encontrado."
+                        );
+                }
+                context.Entry(response).CurrentValues.SetValues(entity);
+                return new Result<PostagemEntity>(
+                    response, 
+                    200, 
+                    "Operação executada com sucesso."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<PostagemEntity>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (UPDATE). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
 }
