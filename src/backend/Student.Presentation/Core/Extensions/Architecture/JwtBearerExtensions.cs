e458123d2d65e15bd761d09cdf761fc070f0e526
Adyllson <domingosady11son@gmail.com>
2025-05-01 17:03:25 +0100

    criando a entity do usuario

--- src/backend/Student.Presentation/Core/Extensions/Architecture/JwtBearerExtensions.cs
+++ src/backend/Student.Presentation/Core/Extensions/Architecture/JwtBearerExtensions.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 namespace Student.Presentation.Core.Extensions.Architecture;
 public static class JwtBearerExtensions
 {
     public static void AddJwtBearerExtensions(this WebApplicationBuilder builder)
     {
-
+        
     }
 }
